9a0481b09f685ee91bb7ddc8ebfce99b1af32f3e
icewall <icew4ll.io>
2018-09-29 11:06:39 -0700

    match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // imports {{{
 extern crate failure;
+// extern crate glob;
 #[macro_use]
 extern crate envconfig_derive;
 extern crate duct;
@@ -14,8 +15,10 @@ extern crate csv;
 // use duct::cmd;
 use envconfig::Envconfig;
 use failure::Error;
+// use glob::glob;
 use std::fs;
 use std::process;
+use structopt::StructOpt;
 // use std::io::{self};
 // use structopt::StructOpt;
 // macro to create vector of strings
@@ -37,14 +40,14 @@ struct Csv {
 // index 0 parsing
 #[derive(Debug, StructOpt)]
 struct Opt {
-    alias: Option<String>,
+    file: Option<String>,
     #[structopt(subcommand)]
     cmd: Option<Command>,
 }
 // subcommand parsing
 #[derive(Debug, StructOpt)]
 enum Command {
-    Push { alias: Option<String> },
+    Push { file: Option<String> },
     Mvq { ips: String },
     Rex { test: String },
 }
@@ -53,23 +56,43 @@ enum Command {
 // }}}
 // main {{{
 fn main() {
+    // Setup data
+    let opt = Opt::from_args();
+    let mut csv = vec![];
+    let mut files = vec![];
     // read csv {{{
     // Initialize config from environment variables
     let config = Config::init().unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
     });
-    // init vec to store csv data
-    let mut csv = vec![];
     if let Err(err) = read(&config.home, &mut csv) {
         println!("error running example: {}", err);
         process::exit(1);
     }
-    if let Err(err) = paths(&config.home) {
+    if let Err(err) = paths(&config.home, &mut files) {
         println!("Error gettings paths {}", err);
         process::exit(1);
     }
-
+    // filter directories
+    let items = files
+        .iter()
+        .map(|x| x.replace("/home/fish/m/vim/", ""))
+        .collect::<Vec<_>>();
+    // check for arg0
+    if let Some(arg0) = opt.file {
+        println!("{}", arg0);
+        let target = items
+            .into_iter()
+            .filter(|i| i.to_string() == arg0)
+            .collect::<Vec<_>>();
+        println!("match {}", target.get(0).expect("Match not found"));
+    }
+    // &items.iter().filter(|x| println!("{:?}", x);
+    // println!("{:?}", &items;
+    //
+// mailq.iter().map(|x| &x.1).for_each(|x| println!("{:?}", x));
+// let q = mailq.iter().map(|x| &x.1).collect::<Vec<_>>();
     //}}}
 }
 // }}}
@@ -87,11 +110,13 @@ fn read(home: &str, csv: &mut Vec<((String, String))>) -> Result<(), Error> {
 }
 // }}}
 // paths
-fn paths(home: &str) -> Result<(), Error> {
+fn paths(home: &str, files: &mut Vec<String>) -> Result<(), Error> {
     let dir = format!("{}{}", home, "/m/vim");
-    let paths = fs::read_dir(dir).unwrap();
+    let paths = fs::read_dir(dir)?;
     for path in paths {
-        println!("{}", path?.path().display())
+        // println!("{}", path?.path().display())
+        files.push(path?.path().to_str().unwrap().to_string());
     }
     Ok(())
 }
+// }}}
